74fb38944076eb018f8d73cc29ddc50157ddcffd
ahmetkardesseven <ahmetkardesseven@gmail.com>
2023-04-19 07:03:09 +0300

    Data printed in tableview

--- Hipo/ViewController.swift
+++ Hipo/ViewController.swift
@@ -7,12 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    @IBOutlet weak var tableView: UITableView!
+        
+        var members: [Members] = []
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            tableView.delegate = self
+            tableView.dataSource = self
+            
+            if let path = Bundle.main.path(forResource: "hipo", ofType: "json") {
+                do {
+                    let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                    let decoder = JSONDecoder()
+                    let hipoJson = try decoder.decode(HipoJson.self, from: data)
+                    members = hipoJson.members
+                    tableView.reloadData()
+                } catch {
+                    print("Error: \(error.localizedDescription)")
+                }
+            }
+        }
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return members.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            let member = members[indexPath.row]
+            cell.textLabel?.text = member.name
+            return cell
+        }
 
 
 }
