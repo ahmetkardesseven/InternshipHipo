72bfbd5a9fb1f20b47dd7532cd2e612f31d0072b
ahmetkardesseven <ahmetkardesseven@gmail.com>
2023-04-20 10:38:48 +0300

    Add AppIcon

--- Hipo/MemberDetailViewController.swift
+++ Hipo/MemberDetailViewController.swift
@@ -8,8 +8,8 @@
 import UIKit
 
 class MemberDetailViewController: UIViewController {
-
-   
+    
+    
     var selectedMember: Members? // selected member passed from previous view controller
     @IBOutlet weak var avatarImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
@@ -18,59 +18,69 @@ class MemberDetailViewController: UIViewController {
     
     @IBOutlet weak var followingLabel: UILabel!
     override func viewDidLoad() {
-            super.viewDidLoad()
-            if let username = selectedMember?.github {
-                let baseURL = "https://api.github.com/users/"
-                guard let url = URL(string: baseURL + username) else {
-                    print("Invalid URL for username: \(username)")
+        super.viewDidLoad()
+        self.navigationItem.title = "Name"
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = UIColor(named: "title")
+        appearance.titleTextAttributes = [.foregroundColor:UIColor(named: "writeColor")!,.font:UIFont(name: "Pacifico-Regular", size: 22)!]
+        navigationController?.navigationBar.barStyle = .black
+        
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
+        if let username = selectedMember?.github {
+            let baseURL = "https://api.github.com/users/"
+            guard let url = URL(string: baseURL + username) else {
+                print("Invalid URL for username: \(username)")
+                return
+            }
+            
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            
+            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                if let error = error {
+                    print("Error: \(error.localizedDescription)")
                     return
                 }
                 
-                var request = URLRequest(url: url)
-                request.httpMethod = "GET"
+                guard let data = data else {
+                    print("Data is nil for username: \(username)")
+                    return
+                }
                 
-                let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-                    if let error = error {
-                        print("Error: \(error.localizedDescription)")
-                        return
-                    }
-                    
-                    guard let data = data else {
-                        print("Data is nil for username: \(username)")
-                        return
-                    }
-                    
-                    do {
-                        if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                            let name = json["name"] as? String ?? "Salih Karasuluoglu"
-                            let followers = json["followers"] as? Int ?? 0
-                            let following = json["following"] as? Int ?? 0
-                            let avatarURL = json["avatar_url"] as? String ?? ""
-                            
-                            DispatchQueue.main.async {
-                                self.nameLabel.text = name
-                                self.followersLabel.text = "\(followers)"
-                                self.followingLabel.text = "\(following)"
-                                if let url = URL(string: avatarURL) {
-                                    do {
-                                        let data = try Data(contentsOf: url)
-                                        self.avatarImageView.image = UIImage(data: data)
-                                    } catch {
-                                        print("Error loading image from URL: \(error.localizedDescription)")
-                                    }
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                        let name = json["name"] as? String ?? "Salih Karasuluoglu"
+                        let followers = json["followers"] as? Int ?? 0
+                        let following = json["following"] as? Int ?? 0
+                        let avatarURL = json["avatar_url"] as? String ?? ""
+                        
+                        DispatchQueue.main.async {
+                            self.nameLabel.text = name
+                            self.followersLabel.text = "\(followers)"
+                            self.followingLabel.text = "\(following)"
+                            if let url = URL(string: avatarURL) {
+                                do {
+                                    let data = try Data(contentsOf: url)
+                                    self.avatarImageView.image = UIImage(data: data)
+                                } catch {
+                                    print("Error loading image from URL: \(error.localizedDescription)")
                                 }
                             }
                         }
-                    } catch {
-                        print("Error parsing JSON for username: \(username)")
                     }
+                } catch {
+                    print("Error parsing JSON for username: \(username)")
                 }
-                
-                task.resume()
             }
-                
-            }
+            
+            task.resume()
         }
-    
-   
+        
+    }
+}
+
+
 
--- Hipo/ViewController.swift
+++ Hipo/ViewController.swift
@@ -8,57 +8,76 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
-  
+    
     @IBOutlet weak var tableView: UITableView!
-        
+    
     @IBOutlet weak var searchMembers: UISearchBar!
     
+    
+    var members: [Members] = []
+    var filteredMembers: [Members] = [] // filtered members list
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "members"
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = UIColor(named: "title")
+        appearance.titleTextAttributes = [.foregroundColor:UIColor(named: "writeColor")!,.font:UIFont(name: "Pacifico-Regular", size: 22)!]
+        navigationController?.navigationBar.barStyle = .black
         
-        var members: [Members] = []
-        var filteredMembers: [Members] = [] // filtered members list
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
         
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            tableView.delegate = self
-            tableView.dataSource = self
-            searchMembers.delegate = self
-            searchMembers.placeholder = "Search"
-            
-            if let path = Bundle.main.path(forResource: "hipo", ofType: "json") {
-                do {
-                    let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
-                    let decoder = JSONDecoder()
-                    let hipoJson = try decoder.decode(HipoJson.self, from: data)
-                    members = hipoJson.members
-                    filteredMembers = members // set the filtered members list with all members initially
-                    tableView.reloadData()
-                } catch {
-                    print("Error: \(error.localizedDescription)")
-                }
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchMembers.delegate = self
+        searchMembers.placeholder = "Search"
+        
+        if let path = Bundle.main.path(forResource: "hipo", ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                let decoder = JSONDecoder()
+                let hipoJson = try decoder.decode(HipoJson.self, from: data)
+                members = hipoJson.members
+                filteredMembers = members // set the filtered members list with all members initially
+                tableView.reloadData()
+            } catch {
+                print("Error: \(error.localizedDescription)")
             }
         }
-        
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return filteredMembers.count // display the count of filtered members list
-        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredMembers.count // display the count of filtered members list
+    }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
         let member = filteredMembers[indexPath.row] // use the filtered members list instead of members
         cell.nameLabel.text = member.name
-           cell.positionLabel.text = member.hipo.position
+        cell.positionLabel.text = member.hipo.position
         return cell
     }
-        
-      
-        
-        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-            
-            filteredMembers = searchText.isEmpty ? members : members.filter({(member: Members) -> Bool in
-                return member.name.lowercased().contains(searchText.lowercased()) || member.hipo.position.lowercased().contains(searchText.lowercased())
-            })
-           
-            tableView.reloadData() // reload the table with filtered results
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            members.remove(at: indexPath.row)
+            filteredMembers = members
+            tableView.deleteRows(at: [indexPath], with: .fade)
         }
+    }
+    
+    
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        filteredMembers = searchText.isEmpty ? members : members.filter({(member: Members) -> Bool in
+            return member.name.lowercased().contains(searchText.lowercased()) || member.hipo.position.lowercased().contains(searchText.lowercased())
+        })
+        
+        tableView.reloadData() // reload the table with filtered results
+    }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let member = filteredMembers[indexPath.row]
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
@@ -69,31 +88,31 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     @IBAction func AddButtonü(_ sender: Any) {
         let alertController = UIAlertController(title: "Add Member", message: nil, preferredStyle: .alert)
-            alertController.addTextField { (textField) in
-                textField.placeholder = "Name"
-            }
-            alertController.addTextField { (textField) in
-                textField.placeholder = "Position"
-            }
-            alertController.addTextField { (textField) in
-                textField.placeholder = "Github"
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Name"
         }
-            let addAction = UIAlertAction(title: "Add", style: .default) { [weak self] (_) in
-                guard let self = self,
-                      let name = alertController.textFields?[0].text,
-                      let position = alertController.textFields?[1].text,
-                      let github = alertController.textFields?[2].text
-                      else { return }
-                let newMember = Members(name: name, github: github, hipo: Hipo(position: position, years_in_hipo: nil))
-                self.members.append(newMember)
-                self.filteredMembers = self.members
-                self.tableView.reloadData()
-            }
-            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-            alertController.addAction(addAction)
-            alertController.addAction(cancelAction)
-            present(alertController, animated: true, completion: nil)
-            
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Position"
+        }
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Github"
+        }
+        let addAction = UIAlertAction(title: "Add", style: .default) { [weak self] (_) in
+            guard let self = self,
+                  let name = alertController.textFields?[0].text,
+                  let position = alertController.textFields?[1].text,
+                  let github = alertController.textFields?[2].text
+            else { return }
+            let newMember = Members(name: name, github: github, hipo: Hipo(position: position, years_in_hipo: nil))
+            self.members.append(newMember)
+            self.filteredMembers = self.members
+            self.tableView.reloadData()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(addAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
+        
         
     }
 }
